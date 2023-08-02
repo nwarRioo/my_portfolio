78a888125228dd019ffd138e6e00696a8fba2578
Сатыбалдинов Ануар <paramode88@gmail.com>
2023-08-02 23:43:03 +0600

    #1 Создание основного компонента-контейнера

---
+++ src/containers/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,13 @@
+import { FC, ReactElement } from "react";
+import styles from "./ProfilePage.module.css";
+
+
+const ProfilePage: FC = (): ReactElement => {
+    return (
+        <div className={styles.profilePage}>
+            ProfilePage
+        </div>
+    );
+};
+
+export default ProfilePage;
