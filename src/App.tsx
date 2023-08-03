00c37d8abeab28a21e477a59167b3e4206aa9ea0
Сатыбалдинов Ануар <paramode88@gmail.com>
2023-08-03 23:21:31 +0600

    #2 Создание хэдера

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { FC, ReactElement } from 'react'
-import ProfilePage from './containers/ProfilePage/ProfilePage'
+import { FC, ReactElement } from "react";
+import ProfilePage from "./containers/ProfilePage/ProfilePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App: FC = (): ReactElement => {
-  return (
-    <>
-      <ProfilePage />
-    </>
-  )
-}
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/my_portfolio" element={<ProfilePage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
--- src/containers/ProfilePage/ProfilePage.tsx
+++ src/containers/ProfilePage/ProfilePage.tsx
@@ -1,16 +1,19 @@
 import { FC, ReactElement } from "react";
 import styles from "./ProfilePage.module.css";
 import { Container } from "../../components/UI/Container/Container";
+import Header from "../../components/Header/Header";
 
 
 const ProfilePage: FC = (): ReactElement => {
     return (
-        <Container>
-            <div className={styles.profilePage}>
-                <h1>ProfilePage</h1>
-                <p className={styles.statusText}>В разработке...</p>
-            </div>
-        </Container>
+        <>  
+            <Header />
+            <Container>
+                <div className={styles.profilePage}>
+                    <p className={styles.statusText}>В разработке...</p>
+                </div>
+            </Container>
+        </>
     );
 };
 
