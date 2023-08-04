b3de7c4d951e586ce6554a3f0535a38d4c30676a
Сатыбалдинов Ануар <paramode88@gmail.com>
2023-08-04 13:49:23 +0600

    #2 Создание компонента Layout

--- src/containers/ProfilePage/ProfilePage.tsx
+++ src/containers/ProfilePage/ProfilePage.tsx
@@ -1,10 +1,11 @@
 import { FC, ReactElement } from "react";
-import styles from "./ProfilePage.module.css";
 import { Container } from "../../components/UI/Container/Container";
 import Header from "../../components/Header/Header";
 import Demo from "./Demo/Demo";
 import Footer from "../../components/Footer/Footer";
-
+import Information from "./Information/Information";
+import { Route, Routes } from "react-router-dom";
+import Layout from "../../components/Layout/Layout";
 
 const ProfilePage: FC = (): ReactElement => {
     return (
@@ -12,10 +13,14 @@ const ProfilePage: FC = (): ReactElement => {
             <Header />
             <Container>
                 <Demo />
-                <div className={styles.profilePage}>
-                    <div className={styles.statusText}>В разработке...</div>
-                    <div className={styles.statusText}>В разработке...</div>
-                </div>
+                <Information />
+                <Routes>
+                    <Route path={"/my_portfolio/"} element={<Layout />}>
+                        <Route path="/my_portfolio/about" element={<Demo />}/>
+                        <Route path="/my_portfolio/works" element={<Demo />}/>
+                        
+                    </Route>
+                </Routes>
             </Container>
             <Footer />
         </>
